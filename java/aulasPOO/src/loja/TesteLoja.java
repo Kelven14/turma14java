4871a8fd42e07f10c51d16f122d483a08a08d4ee
Kelven14 <kelven.cleiton14@gmail.com>
2020-12-18 18:33:32 -0300

    Finalizacao do projeto LOJA

--- java/aulasPOO/src/loja/TesteLoja.java
+++ java/aulasPOO/src/loja/TesteLoja.java
@@ -9,9 +9,18 @@ public class TesteLoja {
 		char opcao = 0, genero;
 		int opcaoMenu = 0, anoNascimento, quantidade;
 		String nome, cpf, codigo;
-		double totalPagamento = 0;
+
 		char opcaoCompra;
 		int opcaoContinua;
+		final double descontoAvista = 0.1;
+		final double jurosAvistaCartao = 0.05;
+		final double jurosParcelado = 0.1;
+		double totalPagamento = 0;
+		double valorParcela = 0.0;
+		double desconto = 0.0;
+		double acrescimo = 0.0;
+		double imposto = 0.0;
+		double totalFinal = 0.0;
 
 		Scanner leia = new Scanner(System.in);
 
@@ -29,9 +38,14 @@ public class TesteLoja {
 		listaProdutos.add(new Produto("Macacão preto", "MM09", 85, 10));
 		listaProdutos.add(new Produto("Croped vermelho", "MM10", 20, 10));
 
-		
-		
 		do {
+			totalPagamento = 0.0;
+			valorParcela = 0.0;
+			desconto = 0.0;
+			acrescimo = 0.0;
+			imposto = 0.0;
+			totalFinal = 0.0;
+
 			inseriLinha(85, "▬");
 			System.out.println("                                 Loja Meury Modas");
 			System.out.println("                        Um novo conceito de Moda Feminina");
@@ -75,7 +89,7 @@ public class TesteLoja {
 
 					boolean encontra = false;
 					boolean passou = false;
-					boolean teste=false;
+					boolean teste = false;
 
 					for (Produto produtos : listaProdutos) {
 
@@ -83,21 +97,19 @@ public class TesteLoja {
 
 							if (quantidade <= produtos.getQtdeProdutoEstoque()) {
 								totalPagamento += produtos.venda(quantidade);
-								
+
 								for (Produto pod : carrinho) {
-									if(codigo.equals(pod.getCodigo())) {
-										teste=true;
+									if (codigo.equals(pod.getCodigo())) {
+										teste = true;
 										pod.adicionaEstoque(quantidade);
 										encontra = true;
 									}
-									
-									
 								}
-								if(teste==false) {
-								
-								carrinho.add(new Produto(produtos.getNomeProduto(), produtos.getCodigo(),
-										produtos.getPrecoUnitario(), quantidade));
-								encontra = true;
+								if (teste == false) {
+
+									carrinho.add(new Produto(produtos.getNomeProduto(), produtos.getCodigo(),
+											produtos.getPrecoUnitario(), quantidade));
+									encontra = true;
 								}
 							} else {
 								passou = true;
@@ -136,7 +148,7 @@ public class TesteLoja {
 							inseriLinha(85, "▬");
 							System.out.printf("             \t\t\t\t\t\tTOTAL DA COMPRA: R$ %.2f", totalPagamento);
 							System.out.println(
-									"\nSelecione uma das opções: \n[1]-Continuar Comprando\n[2]-Remover um produto do carrinhoz\n[3]-Finalizar a compra e emitir a nota fiscal");
+									"\nSelecione uma das opções: \n[1]-Continuar Comprando\n[2]-Remover um produto do carrinho\n[3]-Finalizar a compra e emitir a nota fiscal");
 							opcaoContinua = leia.nextInt();
 							if (opcaoContinua == 1) {
 								opcaoCompra = 'S';
@@ -152,10 +164,11 @@ public class TesteLoja {
 								for (Produto produtos : carrinho) {
 
 									if (codigo.equals(produtos.getCodigo())) {
-										
+
 										if (quantidade < produtos.getQtdeProdutoEstoque()) {
 											totalPagamento -= produtos.venda(quantidade);
-											System.out.printf("Foi removido  %d  produtos do tipo  %s", quantidade,produtos.getNomeProduto());
+											System.out.printf("Foi removido  %d  produtos do tipo  %s", quantidade,
+													produtos.getNomeProduto());
 											pula();
 											encontra = true;
 											for (Produto prod : listaProdutos) {
@@ -169,24 +182,23 @@ public class TesteLoja {
 										} else if (quantidade == produtos.getQtdeProdutoEstoque()) {
 
 											totalPagamento -= produtos.venda(quantidade);
-											System.out.printf("Foi removido  %d  produtos do tipo  %s", quantidade,produtos.getNomeProduto());
+											System.out.printf("Foi removido  %d  produtos do tipo  %s", quantidade,
+													produtos.getNomeProduto());
 											pula();
 											encontra = true;
-											int indice=carrinho.indexOf(produtos);
+											int indice = carrinho.indexOf(produtos);
 											System.out.printf("numero %d ", indice);
 											carrinho.remove(indice);
-								
-											
+
 											for (Produto prod : listaProdutos) {
 
 												if (codigo.equals(prod.getCodigo())) {
 													prod.adicionaEstoque(quantidade);
-												
 
 												}
 											}
 											break;
-										
+
 										} else {
 											passou = true;
 
@@ -202,10 +214,67 @@ public class TesteLoja {
 								}
 
 							} else if (opcaoContinua == 3) {
-								System.out.println("NOTA FISCAL");
-								System.exit(0);
-						
-							
+
+								System.out.print(
+										"Qual a forma de pagamento? \n1-À vista em dinheiro\n2-Cartão de Débito\n3-Cartão de Crédito(uma vez)\n4-Cartão de Crédito(Parcelado em até três vezes)\n");
+								int formaPag = leia.nextInt();
+								if (formaPag == 1) {
+									desconto = totalPagamento * descontoAvista;
+
+								} else if (formaPag == 2) {
+									acrescimo = 0.0;
+
+								} else if (formaPag == 3) {
+									acrescimo = totalPagamento * jurosAvistaCartao;
+								} else if (formaPag == 4) {
+									acrescimo = totalPagamento * jurosParcelado;
+									System.out.print("Quantas Parcelas?");
+									int parcela = leia.nextInt();
+
+									valorParcela = totalPagamento / parcela;
+
+									System.out.printf("Valor total parcelado %d vezes é igual R$ %.2F ", parcela,
+											valorParcela);
+								} else {
+									System.out.print("Opção inválida! Digite uma opção válida\n");
+
+								}
+
+								imposto = (totalPagamento + acrescimo - desconto) * 0.1;
+								totalFinal = totalPagamento + acrescimo - desconto + imposto;
+											                 	                 
+								inseriLinha(85, "▬");
+								System.out.println("            NOTA FISCAL             CÓDIGO DE BARRA   ");
+								inseriLinha(85, "▬");	
+								System.out.println("         __________________        _________________  ");
+								System.out.println("         |                 |       |                | ");
+								System.out.println("         |                 |       | █||█||█||█||█  | ");
+								System.out.println("         |Loja Meury Modas |       | █||█||█||█||█  | ");
+								System.out.println("         |                 |       | █||█||█||█||█  | ");
+								System.out.println("         |_________________|       |________________| ");
+								inseriLinha(85, "▬");
+								System.out.printf("Nome: %s\tCPF: %S\tIdade: %d anos", cliente.getNome(),
+										cliente.getCpf(), cliente.voltaIdade(cliente.getAnoNascimento()));
+								System.out.println();
+								inseriLinha(85, "▬");
+								System.out.println();
+								System.out.println("\nCODIGO\t\tPRODUTO\t\t\tR$(UN)\t\t\tQTDE\t  TOTAL R$");
+								for (Produto produtos : carrinho) {
+
+									System.out.printf("%s\t\t%s\t\t%.2f\t\t\t  %d\t   %.2f\n", produtos.getCodigo(),
+											produtos.getNomeProduto(), produtos.getPrecoUnitario(),
+											produtos.getQtdeProdutoEstoque(),
+											(produtos.getPrecoUnitario() * produtos.getQtdeProdutoEstoque()));
+								}
+								inseriLinha(85, "▬");
+								System.out.printf("             \t\t\t\t\t\t VALOR PARCIAL  : R$ %.2f\n",
+										totalPagamento);
+								System.out.printf("             \t\t\t\t\t\t    JUROS       : R$ %.2f\n", acrescimo);
+								System.out.printf("             \t\t\t\t\t\t    DESCONTOS   : R$ %.2f\n", desconto);
+								System.out.printf("             \t\t\t\t\t\t    IMPOSTO     : R$ %.2f\n", imposto);
+								inseriLinha(85, "▬");
+								System.out.printf("             \t\t\t\t\t\t  VALOR TOTAL   : R$ %.2f\n", totalFinal);
+								break;
 							}
 
 						} while (opcaoContinua != 1);
@@ -257,11 +326,11 @@ public class TesteLoja {
 
 	public static void trataGenero(String nome, char opcao) {
 		if (opcao == 'M') {
-			System.out.printf("\nSeja bem-vindo %s !\n", nome);
+			System.out.printf("\nSeja bem-vindo %s a Loja Meury Modas!\n", nome);
 		} else if (opcao == 'F') {
-			System.out.printf("\nSeja bem-vinda %s !\n", nome);
+			System.out.printf("\nSeja bem-vinda %s a Loja Meury Modas!\n", nome);
 		} else if (opcao == 'X') {
-			System.out.printf("\nSeja bem-vindx %s !\n", nome);
+			System.out.printf("\nSeja bem-vindx %s a Loja Meury Modas !\n", nome);
 		}
 
 	}
